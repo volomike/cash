b9cfe109ac557201e8f204d0d33501208e7a9890
simeydotme <simey.me@gmail.com>
2016-04-17 21:51:19 +0800

    [ collection skeleton ] - add skeleton test for collections

---
+++ test/scripts/collection.js
@@ -0,0 +1,31 @@
+
+/**
+ * dom collection.
+ * tests for changing dom collections
+ * with methods like .add, .filter, .first, .get, etc
+ */
+
+describe( 'dom collections', function() {
+
+    describe( '.add()', function() {
+
+        it( messages.collection , function() {
+
+            var $fixture = $('#traversal-fixture');
+            var $add = $fixture.add('.class-fixture');
+            ( $add ).should.be.an.instanceOf(Object);
+            ( $add ).should.be.an.instanceOf($);
+
+        });
+
+        it( messages.elements(4), function() {
+
+            var $fixture = $('#traversal-fixture');
+            var $add = $fixture.add('.class-fixture');
+            ( $add.length ).should.be.equal( 4 );
+
+        });
+
+    });
+
+});
