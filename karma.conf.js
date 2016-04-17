d619f63bb56caf458d855534070feac2c2f8be19
simeydotme <simey.me@gmail.com>
2016-04-17 19:23:10 +0800

    [ get coverage working ] - add test coverage and reports - add dom selection tests - add browsers for testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,56 @@
+
 module.exports = function(config) {
+
   config.set({
 
     preprocessors: {
-      'test/testmocha.html': ['html2js']
+
+      'test/testmocha.html': ['html2js'],
+      'dist/cash.js': ['coverage']
+
     },
 
-    basePath: '',
-    frameworks: ['mocha', 'should'],
-    //browsers: ['Chrome'],
-    files: [
+    reporters: [
 
-        "dist/cash.js",
-        "test/testmocha.html",
-        "test/karma.test.js"
+        'dots',
+        'coverage'
 
     ],
 
-    singleRun: false,
+    coverageReporter: {
+
+        dir: 'dist/reports/coverage',
+        reporters: [{ type: 'html', subdir: 'report-html' }]
+
+    },
+
+    basePath: '',
+
+    frameworks: [
+
+        'mocha',
+        'should'
+    ],
+
+    browsers: [
+
+        'Chrome',
+        'IE',
+        //'Firefox',
+        //'Opera',
+        //'Safari',
+        'PhantomJS'
+
+    ],
+
+    files: [
+
+        'dist/cash.js',
+        'test/testmocha.html',
+        'test/karma.test.js'
+
+    ],
+
     colors: true
 
   });
