bbb9a0d38fb3bcfab0651605c32fa446d1921219
simeydotme <simey.me@gmail.com>
2016-04-24 15:02:58 +0800

    [ tests for collection ] - add .eq() tests - add .filter() tests

--- test/scripts/collection.js
+++ test/scripts/collection.js
@@ -29,4 +29,93 @@ describe( 'dom collections', function() {
 
     });
 
+    describe( '.eq()', function() {
+
+        it( messages.collection , function() {
+
+            var $fixture = $('#fixtures section');
+            var $eq = $fixture.eq(0);
+            ( $eq ).should.be.an.instanceOf(Object);
+            ( $eq ).should.be.an.instanceOf($);
+
+        });
+
+        it( '.eq(0) should return #selector-fixture', function() {
+
+            var $fixture = $('#fixtures section');
+            var $eq = $fixture.eq(0);
+            ( $eq.length ).should.be.equal( 1 );
+            ( $eq.is('#selector-fixture') ).should.be.true();
+
+        });
+
+        it( '.eq(-1) should return #last-fixture', function() {
+
+            var $fixture = $('#fixtures section');
+            var $eq = $fixture.eq(-1);
+            ( $eq.length ).should.be.equal( 1 );
+            ( $eq.is('#last-fixture') ).should.be.true();
+
+        });
+
+    });
+
+    describe( '.filter()', function() {
+
+        it( messages.collection , function() {
+
+            var $fixture = $('#traversal-fixture div');
+            var $filter = $fixture.filter('.deep');
+            ( $filter ).should.be.an.instanceOf(Object);
+            ( $filter ).should.be.an.instanceOf($);
+
+        });
+
+        describe( '.filter() by classname', function() {
+
+            it( messages.elements(3), function() {
+
+                var $fixture = $('#traversal-fixture div');
+                var $filter = $fixture.filter('.deep');
+                ( $filter.length ).should.be.equal( 3 );
+
+            });
+
+        });
+
+        describe( '.filter() by id', function() {
+
+            it( messages.elements(1), function() {
+
+                var $fixture = $('#traversal-fixture div');
+                var $filter = $fixture.filter('#multiple-id');
+                ( $filter.length ).should.be.equal( 1 );
+
+            });
+
+            it( 'should have "#traversal-fixture" as an ancestor', function() {
+
+                var $fixture = $('#traversal-fixture div');
+                var $filter = $fixture.filter('#multiple-id');
+                var $ancestor = $filter.closest('#traversal-fixture');
+                ( $ancestor.length ).should.be.equal( 1 );
+
+            });
+
+        });
+
+        describe( '.filter() by data-attr', function() {
+
+            it( messages.elements(1), function() {
+
+                var $fixture = $('#traversal-fixture div');
+                var $filter = $fixture.filter('[data-filter]');
+                ( $filter.length ).should.be.equal( 1 );
+
+            });
+
+        });
+
+    });
+
 });
