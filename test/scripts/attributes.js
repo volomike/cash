cd03f5f2dee79f32708e8c0a7b326ba659d23f9b
simeydotme <simey.me@gmail.com>
2016-04-30 16:22:00 +0800

    [ tidy attribute test ] - tidy up addClass - tidy up hasClass - tidy up RemoveClass

--- test/scripts/attributes.js
+++ test/scripts/attributes.js
@@ -7,34 +7,30 @@
 
 describe( 'attributes', function() {
 
-
-
     describe( '.addClass()', function() {
 
-        describe( 'when no argument, an integer, or falsey value', function() {
+        describe( 'when an integer, or falsey value', function() {
 
-            it( 'should return a cash collection, and not change classList' , function() {
+            it( 'should not change classList' , function() {
 
                 var $fixture = $('#addClass1');
-                var initial = $fixture[0].classList.toString();
+                var initial = $fixture[0].className;
 
                 var $empty = $fixture.addClass('');
+                var $integer = $fixture.addClass( 3 );
+                var $null = $fixture.addClass( null );
+                var $undefined = $fixture.addClass( undefined );
+
                 ( $empty ).should.be.an.instanceOf(Object);
                 ( $empty ).should.be.an.instanceOf($);
-
-                var $undefined = $fixture.addClass( undefined );
                 ( $undefined ).should.be.an.instanceOf(Object);
                 ( $undefined ).should.be.an.instanceOf($);
-
-                var $null = $fixture.addClass( null );
                 ( $null ).should.be.an.instanceOf(Object);
                 ( $null ).should.be.an.instanceOf($);
-
-                var $integer = $fixture.addClass( 3 );
                 ( $integer ).should.be.an.instanceOf(Object);
                 ( $integer ).should.be.an.instanceOf($);
 
-                var final = $fixture[0].classList.toString();
+                var final = $fixture[0].className;
 
                 ( final ).should.be.equal( initial );
 
@@ -42,82 +38,52 @@ describe( 'attributes', function() {
 
         });
 
-        describe( 'when run on single element', function() {
+        describe( 'when adding a single class', function() {
 
-            it( messages.collection , function() {
+            it( 'should have the new class' , function() {
 
-                var $fixture = $('#addClass1');
-                var $add = $fixture.addClass('cashtest');
-                ( $add ).should.be.an.instanceOf(Object);
-                ( $add ).should.be.an.instanceOf($);
+                var $fixture1 = $('#addClass1');
+                var $fixture2 = $('#addClass2');
+                var $fixture3 = $('#addClass1, #addClass2');
 
-            });
+                var $add1 = $fixture1.addClass('benjamin');
+                var $add2 = $fixture3.addClass('franklin');
 
-            describe( 'when adding a single class', function() {
+                ( $fixture1.hasClass('benjamin') ).should.be.true();
+                ( $fixture1.hasClass('franklin') ).should.be.true();
+                ( $fixture2.hasClass('franklin') ).should.be.true();
 
-                it( 'should have the new class' , function() {
-
-                    var $fixture = $('#addClass1').addClass('benjamin');
-                    var has = $fixture.hasClass('benjamin');
-                    has.should.be.true();
-
-                });
-
-            });
-
-            describe( 'when adding multiple classes', function() {
-
-                it( 'should have multiple new classes' , function() {
-
-                    var $fixture = $('#addClass1').addClass('frank lin');
-                    var has = ( $fixture.hasClass('frank') && $fixture.hasClass('lin') );
-                    has.should.be.true();
-
-                });
+                ( $add1 ).should.be.an.instanceOf(Object);
+                ( $add1 ).should.be.an.instanceOf($);
+                ( $add2 ).should.be.an.instanceOf(Object);
+                ( $add2 ).should.be.an.instanceOf($);
 
             });
 
         });
 
-        describe( 'when run on collection of elements', function() {
+        describe( 'when adding multiple classes', function() {
 
-            it( messages.collection , function() {
+            it( 'should have the new classes' , function() {
 
-                var $fixture = $('#addClass1, #addClass2');
-                var $add = $fixture.addClass('cashtest');
-                ( $add ).should.be.an.instanceOf(Object);
-                ( $add ).should.be.an.instanceOf($);
+                var $fixture1 = $('#addClass1');
+                var $fixture2 = $('#addClass2');
+                var $fixture3 = $('#addClass1, #addClass2');
 
-            });
+                var $add1 = $fixture1.addClass('cash money');
+                var $add2 = $fixture3.addClass('equals rich');
 
-            describe( 'when adding a single class', function() {
+                ( $fixture1.hasClass('cash') ).should.be.true();
+                ( $fixture1.hasClass('money') ).should.be.true();
+                ( $fixture1.hasClass('equals') ).should.be.true();
+                ( $fixture1.hasClass('rich') ).should.be.true();
+                ( $fixture2.hasClass('equals') ).should.be.true();
+                ( $fixture2.hasClass('rich') ).should.be.true();
 
-                it( 'whole collection should have the new class' , function() {
-
-                    $('#addClass1, #addClass2').addClass('collection');
-                    var $add1 = $('#addClass1');
-                    var $add2 = $('#addClass2');
-                    var has = ( $add1.hasClass('collection') && $add2.hasClass('collection') );
-                    has.should.be.true();
-
-                });
-
-            });
-
-            describe( 'when adding multiple classes', function() {
-
-                it( 'whole collection should have multiple new classes' , function() {
-
-                    $('#addClass1, #addClass2').addClass('jeffer son');
-                    var $add1 = $('#addClass1');
-                    var $add2 = $('#addClass2');
-                    var has = (
-                        ( $add1.hasClass('jeffer') && $add1.hasClass('son') ) &&
-                        ( $add2.hasClass('jeffer') && $add2.hasClass('son') )
-                    );
-                    has.should.be.true();
-
-                });
+                ( $add1 ).should.be.an.instanceOf(Object);
+                ( $add1 ).should.be.an.instanceOf($);
+                ( $add2 ).should.be.an.instanceOf(Object);
+                ( $add2 ).should.be.an.instanceOf($);
 
             });
 
@@ -127,88 +93,55 @@ describe( 'attributes', function() {
 
     describe( '.hasClass()', function() {
 
-        describe( 'when run on single element', function() {
+        describe( 'when checking for a single class', function() {
 
-            describe( 'when checking for a single class', function() {
+            it( 'should return true if class is present' , function() {
 
-                it( 'should return true if class is present' , function() {
+                var has = $('#attribute-fixture').hasClass('attribute-class');
+                has.should.be.true();
 
-                    var has = $('#attribute-fixture').hasClass('attribute-class');
-                    has.should.be.true();
-
-                });
-
-                it( 'should return false if class is missing' , function() {
-
-                    var hasNot = $('#attribute-fixture').hasClass('attribute-classy');
-                    hasNot.should.be.false();
-
-                });
+                has = $('#hasclass1, #hasclass2, #hasclass3').hasClass('z');
+                has.should.be.true();
 
             });
 
-            describe( 'when checking for multiple classes', function() {
+            it( 'should return false if class is missing' , function() {
 
-                it( 'should return true when all classes present' , function() {
+                var hasNot = $('#attribute-fixture').hasClass('attribute-classy');
+                hasNot.should.be.false();
 
-                    var has = $('#attribute-fixture').hasClass('soggy foggy moggy');
-                    has.should.be.true();
-
-                });
-
-                it( 'should return false when any class is missing' , function() {
-
-                    var hasNot = $('#attribute-fixture').hasClass('soggy foggy moggy doggy');
-                    hasNot.should.be.false();
-
-                });
+                hasNot = $('#hasclass1, #hasclass2, #hasclass3').hasClass('moolah');
+                hasNot.should.be.false();
 
             });
 
         });
 
-        describe( 'when checking whole collection', function() {
+        describe( 'when checking for a multiple classes', function() {
 
-            describe( 'when checking for a single class', function() {
+            it( 'should return true if all classes are present' , function() {
 
-                it( 'should return true if class is present on any element' , function() {
+                var has = $('#hasclass1').hasClass('x y');
+                has.should.be.true();
 
-                    var has = $('#hasclass1, #hasclass2, #hasclass3').hasClass('z');
-                    has.should.be.true();
-
-                });
-
-                it( 'should return false if class is missing on all elements' , function() {
-
-                    var hasNot = $('#hasclass1, #hasclass2, #hasclass3').hasClass('pie');
-                    hasNot.should.be.false();
-
-                });
+                has = $('#hasclass1, #hasclass2, #hasclass3').hasClass('a b');
+                has.should.be.true();
 
             });
 
-            describe( 'when checking for multiple classes', function() {
+            it( 'should return false if any classes are missing' , function() {
 
-                it( 'should return true when any element matches all classes' , function() {
+                var hasNot = $('#hasclass1').hasClass('y z');
+                hasNot.should.be.false();
 
-                    var has = $('#hasclass1, #hasclass2, #hasclass3').hasClass('y z');
-                    has.should.be.true();
-
-                });
-
-                it( 'should return false when all elements dont match all classes' , function() {
-
-                    var hasNot = $('#hasclass1, #hasclass2, #hasclass3').hasClass('a b pie');
-                    hasNot.should.be.false();
-
-                });
+                hasNot = $('#hasclass1, #hasclass2, #hasclass3').hasClass('y z');
+                hasNot.should.be.false();
 
             });
 
         });
 
     });
-
 
     describe( '.removeClass()', function() {
 
@@ -251,7 +184,7 @@ describe( 'attributes', function() {
                                     .appendTo( $('attribute-fixture') );
 
                 $fixture.removeClass();
-                should.not.exist( $fixture.className );
+                ( $fixture[0].className ).should.equal( '' );
 
                 ( $fixture ).should.be.an.instanceOf(Object);
                 ( $fixture ).should.be.an.instanceOf($);
@@ -266,14 +199,15 @@ describe( 'attributes', function() {
             it( 'should not have the "a" or "aa" class' , function() {
 
                 var $fixture = $('#removeClass1');
-                var $fixture2 = $('#removeClass1, #removeClass2');
+                var $fixture2 = $('#removeClass2');
+                var $fixture3 = $('#removeClass1, #removeClass2');
 
                 var $remove1 = $fixture.removeClass('a');
-                var $remove2 = $fixture2.removeClass('aa');
+                var $remove2 = $fixture3.removeClass('aa');
 
-                ( $remove1.hasClass('a') ).should.be.false();
-                ( $remove1.hasClass('aa') ).should.be.false();
-                ( $remove2.hasClass('aa') ).should.be.false();
+                ( $fixture.hasClass('a') ).should.be.false();
+                ( $fixture.hasClass('aa') ).should.be.false();
+                ( $fixture2.hasClass('aa') ).should.be.false();
 
                 ( $remove1 ).should.be.an.instanceOf(Object);
                 ( $remove1 ).should.be.an.instanceOf($);
@@ -290,17 +224,18 @@ describe( 'attributes', function() {
             it( 'should not have the "b", "bb", "c" or "cc" classes', function() {
 
                 var $fixture1 = $('#removeClass1');
-                var $fixture2 = $('#removeClass1, #removeClass2');
-                var $remove1 = $fixture1.clone().appendTo('#attribute-fixture');
-                var $remove2 = $fixture2.clone().appendTo('#attribute-fixture');
+                var $fixture2 = $('#removeClass2');
+                var $fixture3 = $('#removeClass1, #removeClass2');
 
-                $remove1.removeClass('b bb');
-                $remove2.removeClass('c cc');
+                var $remove1 = $fixture1.removeClass('b bb');
+                var $remove2 = $fixture3.removeClass('c cc');
 
-                ( $remove1.hasClass('b') ).should.be.false();
-                ( $remove1.hasClass('bb') ).should.be.false();
-                ( $remove2.hasClass('c') ).should.be.false();
-                ( $remove2.hasClass('cc') ).should.be.false();
+                ( $fixture1.hasClass('b') ).should.be.false();
+                ( $fixture1.hasClass('bb') ).should.be.false();
+                ( $fixture1.hasClass('c') ).should.be.false();
+                ( $fixture1.hasClass('cc') ).should.be.false();
+                ( $fixture2.hasClass('c') ).should.be.false();
+                ( $fixture2.hasClass('cc') ).should.be.false();
 
                 ( $remove1 ).should.be.an.instanceOf(Object);
                 ( $remove1 ).should.be.an.instanceOf($);
